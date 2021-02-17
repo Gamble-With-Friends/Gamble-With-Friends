a90625e0fbbce465413bfbb7f20d77a622888ab8
Harout Tatarian <harouttatarian@hotmail.com>
2021-02-17 14:15:18 -0500

    Added Custom HUD and Loading Screen

---
+++ Assets/Scripts/AutoHostClient.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.SceneManagement;
+
+public class AutoHostClient : MonoBehaviour
+{
+    public bool forceOffline;
+
+    [SerializeField] NetworkManager networkManager;
+
+    void Start()
+    {
+        
+       if(!Application.isBatchMode)
+        {
+            if(forceOffline)
+            {
+                Debug.Log("Client Build");
+                networkManager.networkAddress = "localhost";
+                networkManager.StartServer();
+                networkManager.StartClient();
+            } else
+            {
+                networkManager.StartClient();
+            }
+            
+        } else
+        {
+            Debug.Log("Server Build");
+        }
+    }
+
+    public void JoinLocal()
+    {
+        networkManager.networkAddress = "localhost";
+        networkManager.StopClient();
+        networkManager.StartClient();
+    }
+
+    public void HostLocalServer()
+    {
+        networkManager.networkAddress = "localhost";
+        networkManager.StartServer();
+    }
+}
