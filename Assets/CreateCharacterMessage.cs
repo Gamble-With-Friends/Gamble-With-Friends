cf0ed5e6108134d1fa5c11026093dc585bfc9396
Harout Tatarian <harouttatarian@hotmail.com>
2021-02-24 14:58:54 -0500

    Add custom spawn
    The players should now spawn next to the desk random in the reception area

---
+++ Assets/CreateCharacterMessage.cs
@@ -0,0 +1,10 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct CreateCharacterMessage : NetworkMessage
+{
+    public string name;
+    public Color playerColor;
+}
