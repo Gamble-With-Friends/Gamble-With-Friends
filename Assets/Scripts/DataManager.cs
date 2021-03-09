2c643dbc1280c59541ce384894c76e62dd6f823a
Harout Tatarian <harouttatarian@hotmail.com>
2021-03-09 14:20:07 -0500

    Updated Data Manager Class

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Data;
 using System.Data.SqlClient;
+using System;
 
 
 // unsecure implementation for testing connection to DB, need API in future
@@ -20,18 +21,94 @@ public class DataManager : MonoBehaviour
 
     public void Start()
     {
-        SendSQL();
+
     }
 
     public void SendSQL()
     {
         using (SqlConnection db = new SqlConnection(connectionString))
         {
-            SqlCommand cmd = new SqlCommand("INSERT Users VALUES ('TEST_USER','test@test.com','testuser','testing123','1991/12/18','100.00','Online')", db);
+            SqlCommand cmd = new SqlCommand("INSERT Users VALUES ('5d931a30-dff0-44b2-aa0b-079529e43756','test@test.com','testuser','testing123!', 1000.00)", db);
             db.Open();
             cmd.ExecuteNonQuery();
         }
     }
 
+    public void AddUser(string email, string displayName, string password)
+    {
+        using (SqlConnection db = new SqlConnection(connectionString))
+        {
+            SqlCommand cmd = new SqlCommand("INSERT Users VALUES (@userId, @email, @displayName, @password, 1000.00)", db);
 
+            SqlParameter userIdParam = new SqlParameter();
+            userIdParam.ParameterName = "@userId";
+            userIdParam.Value = Guid.NewGuid().ToString();
+            cmd.Parameters.Add(userIdParam);
+
+            SqlParameter emailParam = new SqlParameter();
+            emailParam.ParameterName = "@email";
+            emailParam.Value = email;
+            cmd.Parameters.Add(emailParam);
+
+            SqlParameter userNameParam = new SqlParameter();
+            userNameParam.ParameterName = "@displayName";
+            userNameParam.Value = displayName;
+            cmd.Parameters.Add(userNameParam);
+
+            SqlParameter passwordParam = new SqlParameter();
+            passwordParam.ParameterName = "@password";
+            passwordParam.Value = password;
+            cmd.Parameters.Add(passwordParam);
+
+            db.Open();
+            cmd.ExecuteNonQuery();
+        }
+    }
+
+    public bool DisplayNameExists(string displayName)
+    {
+        using (SqlConnection db = new SqlConnection(connectionString))
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Users WHERE displayName=@displayName", db);
+
+            SqlParameter param = new SqlParameter();
+            param.ParameterName = "@displayName";
+            param.Value = displayName;
+            cmd.Parameters.Add(param);
+
+            db.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public bool LoginUser(string displayName, string password)
+    {
+        using (SqlConnection db = new SqlConnection(connectionString))
+        {
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Users WHERE displayName=@displayName AND password=@password", db);
+
+            SqlParameter userNameParam = new SqlParameter();
+            userNameParam.ParameterName = "@displayName";
+            userNameParam.Value = displayName;
+            cmd.Parameters.Add(userNameParam);
+
+            SqlParameter passwordParam = new SqlParameter();
+            passwordParam.ParameterName = "@password";
+            passwordParam.Value = password;
+            cmd.Parameters.Add(passwordParam);
+
+            db.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                return true;
+            }
+            return false;
+        }
+    }
 }
