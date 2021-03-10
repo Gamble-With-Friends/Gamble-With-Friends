7f2d5b507c0f43b1da4732a1e1c33aaa6ad42ad4
AnthonyChang <mr.anthony.chang@gmail.com>
2021-03-10 16:29:17 -0500

    InventoryUI
    UI for buy and sell complete, need to tie to backend, add mouse over information and player coins.

---
+++ Assets/Scripts/Inventory/InventoryUI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class InventoryUI : MonoBehaviour
+{
+    public Button sellButton;
+    public Image icon;
+
+    public void OnItemClick()
+    {
+        Color shade = icon.color;
+        shade.a = 1f;
+        icon.color = shade;
+        sellButton.interactable = true;
+    }
+
+    public void OnSellClick()
+    {
+        if (sellButton.interactable == true)
+        {
+            Color shade = icon.color;
+            shade.a = 0.3f;
+            icon.color = shade;
+            sellButton.interactable = false;
+        }
+    }
+        // Start is called before the first frame update
+        void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
