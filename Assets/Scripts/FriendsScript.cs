5e7695d366de4f98715fd40912db05b861de84cb
irshyn <ishynkevych@protonmail.com>
2021-03-19 17:55:05 -0400

    Added Friend Tab

---
+++ Assets/Scripts/FriendsScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FriendsScript : MonoBehaviour
+{
+    public GameObject friendCardPrefab;
+
+    List<string> friends = new List<string>
+        {
+            "Harout",
+            "Tony",
+            "Sean",
+            "Mark",
+            "Docker"
+        };
+    // Start is called before the first frame update
+    void Start()
+    {
+        InstantiateFriendCards(friends, transform.position.x, transform.position.y, transform.position.z); // transform.position - position of the parent objectof the script
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenFriendListTab()
+    {
+        
+    }
+
+    private void InstantiateFriendCards(List<string> friends, float posX, float posY, float posZ)
+    {
+        var cardHeight = friendCardPrefab.transform.localScale.y;
+        cardHeight += cardHeight * 0.1f; // space after
+        //friendCardPrefab.transform.SetParent(transform, false);
+
+        foreach (var friend in friends)
+        {            
+            //friendCardPrefab.transform.position = new Vector3(posX, posY, posZ); // set position
+            var card = Instantiate(friendCardPrefab, transform); // instantiate prefab
+            card.transform.localPosition.Set(card.transform.localPosition.x, posY, card.transform.localPosition.z);
+            posY += cardHeight;
+        }        
+    }
+}
---
+++ Assets/Tests/EditTests/DataManagerTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests    
+{
+    public class DataManagerTests
+    {
+        [Test]
+        public void DisplayNameExists_True()
+        {
+            string displayName = "test1234";
+            bool exists = DataManager.DisplayNameExists(displayName);
+            Assert.False(exists);
+        }
+
+        public void DisplayNameExists_False()
+        {
+            string displayName = "test123456789";
+            displayName = "testuser";
+            bool exists = DataManager.DisplayNameExists(displayName);
+            Assert.True(exists);
+        }
+
+        [Test]
+        public void LoginUser_IncorrectPassword()
+        {
+            string displayName = "testuser";
+            string password = "1234";
+            var loggedIn = DataManager.LoginUser(displayName, password);
+            Assert.IsNull(loggedIn);
+        }
+
+        [Test]
+        public void LoginUser_UserNameNotExists()
+        {
+            string displayName = "testuser123456789";
+            string password = "testing123!";
+            var loggedIn = DataManager.LoginUser(displayName, password);
+            Assert.IsNull(loggedIn);
+        }
+
+        [Test]
+        public void LoginUser_Successful()
+        {
+            string displayName = "testuser";
+            string password = "testing123";
+            var loggedIn = DataManager.LoginUser(displayName, password);
+            Assert.IsNotNull(loggedIn);
+        }
+    }
+}
