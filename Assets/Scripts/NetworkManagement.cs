2b517cc789c795ea9de2d1fa0bd0a1b951b3ab67
Harout Tatarian <harouttatarian@hotmail.com>
2021-04-07 05:55:00 -0400

    Removed unused functions

--- Assets/Scripts/NetworkManagement.cs
+++ Assets/Scripts/NetworkManagement.cs
@@ -7,20 +7,9 @@ public class NetworkManagement : NetworkManager
     private string serverId;
     
     [Server]
-    public override void OnServerConnect(NetworkConnection conn)
-    {
-        base.OnServerConnect(conn);
-        Debug.Log("OnServerConnect ConnectionId: " + conn.connectionId);
-        Debug.Log("OnServerConnect identity: " + conn.identity);
-    }
-
-    [Server]
     public override void OnServerDisconnect(NetworkConnection conn)
     {
         base.OnServerDisconnect(conn);
-        Debug.Log("OnServerDisconnect ConnectionId: " + conn.connectionId);
-        Debug.Log("OnServerDisconnect identity: " + conn.identity);
-        
         LobbyInfo.GetInstance().LogoutUser(conn.connectionId);
     }
 
@@ -29,7 +18,6 @@ public class NetworkManagement : NetworkManager
     {
         base.OnStartServer();
         serverId = GUID.Generate().ToString();
-        Debug.Log("OnStartServer ServerID: " + serverId);
     }
 
     [Server]
